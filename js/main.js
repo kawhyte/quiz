60914113164b26432cfb4079bcc642949a77a236
Kenny Whyte <kawhyte@gmail.com>
2019-11-29 18:25:18 +0900

    CSS for correct answer

--- js/main.js
+++ js/main.js
@@ -68,18 +68,25 @@ getNewQuestion= ()=>{
       availableQuesions.splice(questionIndex, 1);
       console.log(availableQuesions);
       acceptingAnswers = true;
-      
+
       choices.forEach(choice => {
         choice.addEventListener("click", e => {
           if (!acceptingAnswers) return;
       
           acceptingAnswers = false;
-          const selectedChoice = e.target;
-          const selectedAnswer = selectedChoice.dataset["number"];
-          console.log(selectedAnswer);
-          getNewQuestion();
-        });
-      });
-}
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+}
 startGame();
