fe61cb98576a57d5ae0e9d8b08a91e9bdc48344e
Kenny Whyte <kawhyte@gmail.com>
2019-12-04 01:14:41 +0900

    display top high scores

--- js/end.js
+++ js/end.js
@@ -4,6 +4,9 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore.innerText = mostRecentScore;
 
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
 });
@@ -11,4 +14,17 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
   console.log("clicked the save button!");
   e.preventDefault();
+
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+//   window.location.assign("/");
+
 };
